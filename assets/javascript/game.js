83ee67352fda23cc973732fe2cd0647ee3479118
Diana Mayhugh <dianamayhugh@gmail.com>
2020-04-15 16:04:03 -0400

    notes on steps for the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,28 @@
+//1.Gather user input
+//2.Check for all uppercase and lowercase letters
+//3. Trigger an even listener if pressed key matched
+//4. Generate a random letter  from all letters
+//5. Compare value of user input to generated random value
+//6. if  values are equal, increment the "wins" variable and update in the DOM/HTML element
+//7. if values are not equal, increment the "losses" variable and update in DOM element
+
 var targetLetter;
-var wins;
-var losses;
-var guessesLeft;
-var guessesSoFar;
+var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var charactersLength = characters.length;
+var wins = 0;
+var losses = 0;;
+var guessesLeft = 0;
+var guessesSoFar = 0;
+var gameElement = document.getElementById('game');
+
+//used stack overflow for this function:
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+ }
